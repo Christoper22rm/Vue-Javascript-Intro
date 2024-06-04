bc89ef2a209c2e25aa0c5d22ee15a99d4c5020c7
Christoper <christoper22rm@gmail.com>
2024-06-04 16:07:06 -0500

    siguiente capitulo que me toca es de objetos

---
+++ js/02.js
@@ -0,0 +1,11 @@
+// Variables con const
+
+const cliente = 'juanuan'
+
+// Siempre deben tener un valor al inicializar una variable con const
+
+const precio = 300;
+
+console.log(precio);
+
+console.log(cliente)
---
+++ js/03.js
@@ -0,0 +1,47 @@
+// undifed
+let cliente;
+console.log(cliente);
+
+// tipo de dato boolean
+
+const client = true;
+console.log(typeof client);
+
+//numero en Javascript
+const numero1 = 200;
+const numero2 = 300;
+
+suma = numero1 + numero2;
+console.log(suma);
+
+// tipo de dato string 
+
+const tip = 'cliente'
+console.log(tip);
+
+//bigIn
+
+const numeroGrande = BigInt(1)
+console.log(numeroGrande);
+const numeroPequeño= 30;
+
+console.log(Number(numeroGrande) + numeroPequeño);
+
+// symbols siempre seran diferentes
+const primerSymbol = Symbol(1);
+const segundoSymbol = Symbol(1);
+
+console.log(primerSymbol === segundoSymbol);
+
+// Object
+//array
+const numeros = [1, 2, 3, 4, 5, 6, 7];
+const descuento = null;
+const alumno = {};
+
+console.log(typeof numeros);
+console.log(typeof descuento);
+console.log(typeof alumno);
+
+
+
