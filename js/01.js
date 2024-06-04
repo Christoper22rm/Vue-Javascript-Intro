33073df146d7c37f18d88f69fd1d12b02cc302d7
Christoper <christoper22rm@gmail.com>
2024-06-03 22:12:56 -0500

    iniciando

---
+++ js/01.js
@@ -0,0 +1,17 @@
+// Variables con let
+let cliente = "Juan";
+
+//se puede re-asignar
+cliente = 20
+cliente = true
+
+// pueden iniciar sin valor
+
+let precio
+
+precio = 300
+
+
+
+console.log(cliente);
+console.log(precio);
